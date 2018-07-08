7bd69718404051b642f0dd0ccca4ed23887f84ce
Muhun Kim <iam@muhun.kim>
2018-07-08 15:50:02 +0900

    Using dict instead of if-statements

--- jejunuMeals/__init__.py
+++ jejunuMeals/__init__.py
@@ -6,10 +6,24 @@ from bs4 import BeautifulSoup as bs
 
 URL = 'http://www.jejunu.ac.kr/camp/stud/foodmenu'
 
+YAML = {0: {'점심': {}, '저녁': {}}, 1: {'점심': {}, '저녁': {}}, 2: {'점심': {}, '저녁': {}},
+        3: {'점심': {}, '저녁': {}}, 4: {'점심': {}, '저녁': {}}}
+
+WEEKDAYS = {0: 0, 1: 0, 2: 0, 3: 0,
+            4: 1, 5: 1, 6: 1, 7: 1,
+            8: 2, 9: 2, 10: 2, 11: 2,
+            12: 3, 13: 3, 14: 3, 15: 3,
+            16: 4, 17: 4, 18: 4, 19: 4}
+
+FLAGS = {0: 2, 4: 2, 8: 2, 12: 2, 16: 2}
+
 class JejunuMeals:
 
-    def __init__(self, url=URL):
+    def __init__(self, url=URL, yaml=YAML, weekdays=WEEKDAYS, flags=FLAGS):
         self.url = url
+        self.yaml = yaml
+        self.weekdays = weekdays
+        self.flags = flags
 
     def soup(self, url):
         return bs(get(url).text, 'html.parser')
@@ -19,30 +33,18 @@ class JejunuMeals:
         table_data = [[cell.text for cell in
                       row.select('td.border_right.txt_center')]
                       for row in soup.select('table > tr')]
+        table_data = table_data[1:21]
         return table_data
 
     def fetch_meals(self):
-        toYaml = {(0): {'점심': {}, '저녁': {}}, 1: {'점심': {}, '저녁': {}}, 2: {'점심': {}, '저녁': {}}, 3: {'점심': {}, '저녁': {}}, 4: {'점심': {}, '저녁': {}}}
-        weekday = -1
         data = self.table_tds()
+        yaml = self.yaml
         for index, atom in enumerate(data):
-            if index == 0:
-                continue
-            if index == 1 or index == 5 or index == 9 or index == 13 or index == 17:
-                weekday = weekday + 1
-                toYaml[weekday]['점심']['정식'] = atom[2]
-                toYaml[weekday]['저녁']['정식'] = atom[3]
-            elif index == 2 or index == 6 or index == 10 or index == 14 or index == 18:
-                toYaml[weekday]['점심']['특식'] = atom[1]
-                toYaml[weekday]['저녁']['특식'] = atom[2]
-            elif index == 3 or index == 7 or index == 11 or index == 15 or index == 19:
-                toYaml[weekday]['점심']['양식'] = atom[1]
-                toYaml[weekday]['저녁']['양식'] = atom[2]
-            elif index == 4 or index == 8 or index == 12 or index == 16 or index == 20:
-                toYaml[weekday]['점심']['중식'] = atom[1]
-                toYaml[weekday]['저녁']['중식'] = atom[2]
-            else:
-                return toYaml
+            weekday = self.weekdays[index]
+            flag = self.flags.get(index, 1)
+            yaml[weekday]['점심'][atom[flag - 1]] = atom[flag]
+            yaml[weekday]['저녁'][atom[flag - 1]] = atom[flag + 1]
+        return yaml
 
 if __name__ == '__main__':
     from pprint import pprint
