63f05c799254c6589419853ba53109978fcef77d
Muhun Kim <iam@muhun.kim>
2018-07-21 14:33:35 +0900

    Release v1.0.21

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='jejunuMeals',
-    version='1.0.20',
+    version='1.0.21',
     url='https://github.com/BetaF1sh/jejunuMeals',
     license='GPL v3.0',
     author='BetaF1sh',
     author_email='iam@muhun.kim',
     description='Jeju National University meal data crawler',
+    long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
     python_requires='>=3',
